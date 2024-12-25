bcbc774a666cd5ac7a0dab9128eb263871e0cf52
michael88-lm <mixail29.09.2021@yandex.by>
2024-12-25 17:21:56 +0300

    last commit2

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -3,9 +3,9 @@ import s from "./Greeting.module.css";
 
 type GreetingPropsType = {
   name: string; // need to fix any
-  setNameCallback:(e: ChangeEvent<HTMLInputElement>) => void ; // need to fix any
-  addUser: ()=> void; // need to fix any
-  onBlur: ()=> void; // need to fix any
+  setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void; // need to fix any
+  addUser: () => void; // need to fix any
+  onBlur: () => void; // need to fix any
   onEnter: (e: KeyboardEvent<HTMLInputElement>) => void; // need to fix any
   error: string; // need to fix any
   totalUsers: number; // need to fix any
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
     lastUserName,
   } // деструктуризация пропсов
 ) => {
-  const inputClass = s.errorInput; // need to fix with (?:)
+  const inputClass = error ? `${s.input} ${s.errorInput}` : s.input; // need to fix with (?:)
 
   return (
     <div id={"hw3-form"} className={s.greetingForm}>
@@ -45,9 +45,11 @@ const Greeting: React.FC<GreetingPropsType> = (
             onKeyDown={onEnter}
             onBlur={onBlur}
           />
-          <div id={"hw3-error"} className={s.error}>
-            {error}
-          </div>
+          {error && (
+            <div id="hw3-error" className={s.error}>
+              {error}
+            </div>
+          )}
         </div>
 
         <button
